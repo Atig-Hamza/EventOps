bb340c742b54f8af9dee02bd2a5cad3f08731aed
Atig-Hamza <hamzaatig@icloud.com>
2026-02-04 14:09:59 +0000

    feat: add enums for Role, EventStatus, and ReservationStatus

---
+++ backend/src/common/enums.ts
@@ -0,0 +1,17 @@
+export enum Role {
+    Admin = 'ADMIN',
+    Participant = 'PARTICIPANT',
+}
+
+export enum EventStatus {
+    Draft = 'DRAFT',
+    Published = 'PUBLISHED',
+    Canceled = 'CANCELED',
+}
+
+export enum ReservationStatus {
+    Pending = 'PENDING',
+    Confirmed = 'CONFIRMED',
+    Refused = 'REFUSED',
+    Canceled = 'CANCELED',
+}
